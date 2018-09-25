830a0aa1616814cf280393f7b423f2d6cfc05729
Benjamin <benjaminsalasek@Athena.local>
2018-09-24 21:10:36 -0500

    sorted

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 
+
+longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2)
+
+
+
+function longestConsec(strarr, k) {
+	var n = strarr.length;
+	if (n = 0 || k > n || k <= 0) {return ""}
+
+	var slicedArray = strarr.slice(0, n-k+1);
+	
+	slicedArray.sort(function(a, b){
+		return b.length - a.length
+	});
+//	console.log(slicedArray);
+	var longestString = slicedArray[0];
+		console.log(longestString);
+
+	
+	return slicedArray;
+}
+
+
+
+
+//
+//You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+//
+//#Example: longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+//
+//n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
